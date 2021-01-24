cc0d598b5efd314c05047de0acf90a81f8a15827
Rowan Gordon <rowan@rowangordon.co.uk>
2021-01-24 16:33:54 +0000

    customised dark and light mode colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
 import 'prismjs/themes/prism-okaidia.css';
+import BFLogo from '../images/Bald_Flavours_Logo.png'
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -19,13 +20,18 @@ export default ({ children }) => {
     <div className="site-wrapper">
       <header className="site-header">
         <div className="site-title">
-          <Link to="/">{data.site.siteMetadata.title}</Link>
+          <Link to="/">{/*{data.site.siteMetadata.title}*/}
+            <img 
+              src={BFLogo} 
+              style={{width: '5rem'}}
+            />
+          </Link>
         </div>
         <Navigation />
       </header>
       {children}
       <footer className="site-footer">
-        <p>&copy; {new Date().getFullYear()} Delog &bull; Crafted with <span role="img" aria-label="love">❤️</span> by <a href="https://w3layouts.com">W3Layouts</a></p>
+        <p>&copy; {new Date().getFullYear()} Bald Flavours &bull; Created by <a href="https://rowangordon.co.uk" target="_blank">Rowan Gordon</a></p>
       </footer>
     </div>
   )
