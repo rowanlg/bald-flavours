88c81e6d9dfbd7c997a3f09455390d10a503d314
Rowan Gordon <rowan@rowangordon.co.uk>
2021-01-24 22:15:13 +0000

    added about page, using metadata for customisation

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+import AboutPageContent from "../components/aboutPageContent"
+
+const AboutPage = ({
+  data: {
+    site,
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>{`About | ${site.siteMetadata.title}`}</title>
+        <meta name="description" content={site.siteMetadata.description} />
+      </Helmet>
+      <AboutPageContent />
+    </Layout>
+  )
+}
+
+export default AboutPage
+export const pageQuery = graphql`
+  query aboutPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
