f99ef1698e7ceeb69904d5b68bb7acda9bfd10bb
Rowan Gordon <rowan@rowangordon.co.uk>
2021-01-25 19:57:37 +0000

    started movile navigation toggle

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,10 +2,37 @@ import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
-export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
-  </nav>
-  
-)
+const Navigation = (props) => {
+  return (
+    <>
+      <nav className="navigation"> 
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/reviews">Reviews</Link>
+        <Link to="/homecooking">Home Cooking</Link>
+        <Link to="/copywriting">Copywriting</Link>
+        <Link to="/contact">Contact</Link>
+        <ThemeChanger/>
+      </nav>
+      <div 
+        className="toggle-mobile-nav"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <nav 
+        className="mobile-navigation"
+      > 
+        <Link className="nav-link" to="/">Home</Link>
+        <Link className="nav-link" to="/about">About</Link>
+        <Link className="nav-link" to="/reviews">Reviews</Link>
+        <Link className="nav-link" to="/homecooking">Home Cooking</Link>
+        <Link className="nav-link" to="/copywriting">Copywriting</Link>
+        <Link className="nav-link" to="/contact">Contact</Link>
+        <ThemeChanger />
+      </nav>
+    </>
+  )
+}
+export default Navigation
